94fd780e28986ded95995d0ad176d02cab51a7cb
BernardoLangmachado <130462210+BernardoLangmachado@users.noreply.github.com>
2024-10-21 12:29:32 -0300

    atualizado

---
+++ mobile/spotyfake/app/indes.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react"
+import { View, Text, StyleSheet, Image, Button, ScrollView, Pressable } from "react-native"
+
+import { Link } from "expo-router";
+
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20
+    },
+
+    quadrado: {
+        width: 320,
+        height: 460,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f0f0f0',
+        borderRadius: 20,
+        justifyContent: 'center'
+    },
+
+    buttonsContainer: {
+        flexDirection: 'colon',
+        justifyContent: 'space-between',
+        width: '70%',
+    },
+
+    botaoderegistro: {
+        backgroundColor: '#720e00',
+        borderRadius: 20,
+        height: 54,
+        width: 120,
+        alignItems: 'center',
+        justifyContent: 'center'
+
+    },
+});
+
+
+export default registrar = () => {
+    const [nome, onChangeNome] = React.useState();
+    const [sombrenome, onChangeSombrenome] = React.useState();
+    const [email, onChangeEmail] = React.useState();
+    const [senha, onChangeSenha] = React.useState();
+    const [dataNascimento, onChangeDataNascimento] = React.useState();
+
+    return (
+
+        <View style={styles.container}>
+
+            <View style={styles.quadrado}>
+
+                <TextInput
+                    style={styles.input}
+                    onChangeText={onChangeNome}
+                    value={nome}
+                    placeholder="Escreva seu nome"
+                />
+
+                <TextInput
+                    style={styles.input}
+                    onChangeText={onChangeSombrenome}
+                    value={sombrenome}
+                    placeholder="Escreva seu Sobre nome"
+                />
+
+                <TextInput
+                    style={styles.input}
+                    onChangeText={onChangeEmail}
+                    value={email}
+                    placeholder="Escreva seu Email"
+                />
+
+                <TextInput
+                    style={styles.input}
+                    onChangeText={onChangeSenha}
+                    value={senha}
+                    placeholder="Escreva seu Senha"
+                />
+
+                <TextInput
+                    style={styles.input}
+                    onChangeText={onChangeDataNascimento}
+                    value={dataNascimento}
+                    placeholder="Escreva sua Data de Nascimento"
+                />
+
+                <Pressable style={styles.botaoderegistro}>
+
+                </Pressable>
+
+            </View>
+        </View>
+
+
+
+    );
+};
+
--- server/index.js
+++ server/index.js
@@ -1,9 +1,28 @@
-import { createServer } from 'node:http'
+import  Express  from "express";
 
-const servidor = createServer((req, res) => {
-    console.log('qualquer coisa')
-    res.write('ta funcionando2')
-    return res.end()
+const app = Express()
+app.use(Express.json())
+
+app.post('/resgistro', (req,res) => {
+    const{nome, sobrenome, email, senha, dataNascimento} =req.body
+    if(!nome || !sobrenome || !email || !senha || !dataNascimento){
+        res.send('voce deve preencher todos os campos')
+    } 
+    console.log(email)
+    res.send('user criado')
 })
 
-servidor.listen(8000)
+app.post('/login', (req,res) => {
+    const{ email, senha, } =req.body
+    if( !email || !senha ){
+        res.send('voce deve preencher todos os campos')
+        return
+    } 
+    res.send('Login feito')
+})
+
+
+
+
+app.listen(8000)
+
