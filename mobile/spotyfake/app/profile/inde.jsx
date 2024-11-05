be98dad3920720fc36bfb6a0cf9b219e1cbb390f
BernardoLangmachado <130462210+BernardoLangmachado@users.noreply.github.com>
2024-11-05 10:48:44 -0300

    teste

---
+++ mobile/spotyfake/app/profile/inde.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, View, Text, Image } from 'react-native'
+
+export default profile = () => {
+
+    return(
+        <View style={Styles.container}>
+            <image></image>
+            <View>
+
+            </View>
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    container: {
+        
+    }
+})
